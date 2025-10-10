b539449c9c25215378281ef5d66690584bcd653f
(Sherline Bertrand) <sherlinebertrand@gmail.com>
2025-10-09 22:39:23 -0400

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#find"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
